e4856e04de5674ed8609feaad63e4c9f03f966b8
mcarneirobug <mcarneiro.rs@gmail.com>
2024-07-19 11:20:57 +0100

    feat: added tests and create methods to introduce flux and mono behaviors.

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,31 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("John", "Alice", "Bob", "Charlie", "David"))
+                .log();
+    }
+
+    public Mono<String> nameMono() {
+        return Mono.just("John");
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService service = new FluxAndMonoGeneratorService();
+
+        System.out.println(" ----- Flux ------ ");
+
+        service.namesFlux().subscribe(System.out::println);
+
+        System.out.println(" ----- Mono ------ ");
+
+        service.nameMono().subscribe(System.out::println);
+    }
+
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,36 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
 
+    FluxAndMonoGeneratorService service = new FluxAndMonoGeneratorService();
+
+    /*
+     * .create() is used to takes care of invoking the subscribe call which automatically.
+     */
+    @Test
+    void generateNamesFlux() {
+        // given
+
+        // when
+        var namesFlux = service.namesFlux();
+
+        // then
+        StepVerifier.create(namesFlux)
+                .expectNext("John", "Alice", "Bob", "Charlie", "David")
+                .verifyComplete();
+
+        StepVerifier.create(namesFlux)
+                .expectNextCount(5)
+                .verifyComplete();
+
+        StepVerifier.create(namesFlux)
+                .expectNext("John")
+                .expectNextCount(4)
+                .verifyComplete();
+    }
 }
